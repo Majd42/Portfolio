281473158bab1280b89094cb6fed1e91a5cb73b9
Majd <majd4.sh@gmail.com>
2024-11-19 20:01:53 +0400

    updating resume

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -70,7 +70,7 @@ const Hero = () => {
 
           <div className="flex w-full relative border-t-[0.1px] border-disabled border-opacity-40">
             <Link
-              href={"/Majd-Shaheen-Resume.pdf"}
+              href={"/Majd-Shaheen-CV.pdf"}
               download
               className="text-center w-[50%] p-3 border-r-[0.5px] border-disabled border-opacity-40 font-bold flex justify-center items-center gap-3 "
             >
